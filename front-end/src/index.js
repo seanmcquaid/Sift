a2107a7d168c2876504102bd219ebb366c38eb55
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-02 13:53:04 -0500

    set up redux and dependencies on front end!

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux set up
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware} from "redux";
+import reducers from "./reducers/rootReducer.js";
+import reduxPromise from "redux-promise";
+
+const middleware = applyMiddleware(reduxPromise);
+const theStore = middleware(createStore);
+const theStoreWithMiddlewareAndReducers = theStore(reducers)
+
+
+ReactDOM.render(
+    <Provider store={theStoreWithMiddlewareAndReducers}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
