3af213c49aafe0276d7897737c4fd5f4e135a4ba
Katie Duane <katiejduane@gmail.com>
2019-03-06 13:13:10 -0500

    adjust for list component

--- front-end/src/components/Lists/ToDo/List.js
+++ front-end/src/components/Lists/ToDo/List.js
@@ -6,18 +6,17 @@ class List extends Component {
 
     render(){
         return (
-            <div className="ToDo" >
+            <div className="List" >
                 <table className="Table">
                     <thead>
                         <tr>
                             <th col1={this.props.col1}>{this.props.col1}</th>
                             <th col2={this.props.col2}>{this.props.col2}</th>
                             <th col3={this.props.col3}>{this.props.col3}</th>
-                            <th col4={this.props.col4}>{this.props.col4}</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {this.props.toDoList}
+                        {this.props.list}
                     </tbody>
                 </table>
             </div>
