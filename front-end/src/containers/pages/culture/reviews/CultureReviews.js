cd94cc5d0033d1d450dfcba2c4ee351c1abd543f
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-07 15:35:36 -0500

    finished culture favorites

--- front-end/src/containers/pages/culture/reviews/CultureReviews.js
+++ front-end/src/containers/pages/culture/reviews/CultureReviews.js
@@ -1,50 +1,121 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import AddReviewForm from '../../../Forms/AddReviewForm';
+import SweetAlert from 'sweetalert-react';
+import 'sweetalert/dist/sweetalert.css';
+import { connect } from "react-redux";
+import "./CultureReviews.css";
+import PlaceCards from '../../../../components/Lists/PlaceCards/PlaceCards';
+import Button from "../../../../components/utility/button/Button";
 
 class CultureReviews extends Component {
     constructor() {
         super()
         this.state = {
-            activity: '',
-            stars: '',
-            review: ''
-
+            list : [],
+            msg : "",
+            types : [],
+            showAlert: false,
         }
     }
 
+    componentDidMount(){
+        axios({
+            method : "POST",
+            url : `${window.apiHost}/culture/getCultureReviews`,
+            data : {
+                email : this.props.login.email
+            }
+        }).then((reviewListFromDB)=>{
+            // console.log(reviewListFromDB)
+            this.setState(({
+                list : reviewListFromDB
+            }))
+        })
+    }
+
+    // need to finish add review
+    addReview = (place, review, type, stars) =>{
+        axios({
+            method : "POST",
+            url : `${window.apiHost}/culture/addCultureReview/${place}`,
+            data : {
+                email : this.props.login.email,
+                place,
+                review,
+                type,
+                stars
+            }
+        }).then((responseFromDB)=>{
+            // console.log(responseFromDB)
+            this.setState({
+                list : responseFromDB,
+                msg : `Congrats! You've added a review for ${place}!`,
+                showAlert: true,
+            })
+        })
+    }
+
+    editReview = (dong)=>{
+
+    }
+
+    deleteReview = ()=>{
+        
+    }
+
     render() {
+        if (this.state.list.data !== undefined) {
+            var cultureReviews = this.state.list.data.map((review, i) => {
+                return (
+                    <div key={i} className="placeCard">
+                        <div className="cardLeft">
+                            <h4>{review.placename}</h4>
+                            <p>{review.review}</p>
+                        </div>
+                        <div className="stars">
+                            <p> {review.stars} Stars</p>
+                        </div>
+                        <div className="buttonContainer">
+                            <Button className="shareButton">Share</Button>
+                            <Button clicked={() => this.editReview(review.placename)} className="editButton">Edit</Button>
+                            <Button clicked={() => this.removeReview(review.placename)} className="deleteButton">Remove</Button>
+                        </div>
+                        
+                    </div>
+                )
+            })
+        }
+
+        const typeArray = this.state.types.map((type, i)=>{
+            return (<option key={i} value={type}>{type}</option>)
+        });
+
         return (
-            <div className="CultureReview">
-                <div>
-                    <form onSubmit={this.addCultureReview} className="ReviewForm">
-                        <input onChange={this.changeCultureName} type="text" id="NewReview" placeholder="Place to review" value={this.state.activity} />
-                        <input onChange={this.changeCultureReview} type="text" id="ReviewText" placeholder="Type review here!" value={this.state.review} />
-                        <select className="Dropdown CultureType">
-                            <option default value="Museum/Gallery" onClick={}>Museum/Gallery</option>
-                            <option value="Performance" onClick={}>Performance</option>
-                            <option value="Movies/Music" onClick={}>Movies/Music</option>
-                            <option value="Literature/Learning" onClick={}>Literature/Learning</option>
-                        </select>
-                    </form>
-                </div>
-                <div>
-                    <table className="Table">
-                        <thead>
-                            <tr>
-                                <th>Place</th>
-                                <th>Review</th>
-                                <th>Stars</th>
-                                <th>Remove</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* JSX for mapped array of activities! */}
-                        </tbody>
-                    </table>
-                </div>
+            <div className="CultureReviews">
+                <h2>Reviews</h2>
+                <SweetAlert
+                    show={this.state.showAlert}
+                    title="Added to Faves"
+                    text={this.state.msg}
+                    onConfirm={() => this.setState({ showAlert: false })}
+                />
+                <AddReviewForm
+                    placeholder="Add your food review here!"
+                    defaultType= "Choose type!"
+                    types={typeArray}
+                    addReview={this.addReview}
+                />
+                <PlaceCards cards={cultureReviews}/>
             </div>
         )
     }
 }
 
+function mapStateToProps(state) {
+    return {
+        login: state.login
+    }
+}
 
-export default CultureReviews;
+export default connect(mapStateToProps,null)(CultureReviews);
