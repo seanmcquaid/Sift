93ca18f850e49b48a68bb50715f3d272440c4ba6
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-07 15:22:37 -0500

    finished initial structure for culture to do and favorites'

--- front-end/src/containers/pages/culture/favorites/CultureFavorites.js
+++ front-end/src/containers/pages/culture/favorites/CultureFavorites.js
@@ -1,48 +1,120 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { connect } from "react-redux";
+import AddForm from '../../../Forms/AddForm';
+import PlaceCards from '../../../../components/Lists/PlaceCards/PlaceCards'
+import Button from '../../../../components/utility/button/Button'
+import "./CultureFavorites.css";
 
 class CultureFavorites extends Component {
-    constructor() {
-        super()
-        this.state = {
-            favorite: '',
-            type: '',
-            note: ''
+        constructor() {
+            super()
+            this.state = {
+                list: [],
+                msg: "",
+                showAlert: false,
+            }
+        }
+    
+        componentDidMount() {
+            // console.log('component did mount')
+            axios({
+                method: 'POST',
+                url: `${window.apiHost}/culture/getCultureFaveList`,
+                data: {
+                    email: this.props.login.email
+                }
+            }).then((cultureListFromDB) => {
+                    this.setState({
+                        list: cultureListFromDB
+                    })
+                })
+        }
+    
+        addNewPlace = (place, type, text) => {
+            //api call will go here with autocomplete to add name, location to DB
+            // console.log(place, type)
+            axios({
+                method: 'POST',
+                url: `${window.apiHost}/culture/addFaveInFavorites`,
+                data: {
+                    placename: place,
+                    type: type,
+                    note: text,
+                    email: this.props.login.email
+                }
+            }).then((backEndResponse) => {
+                // console.log(backEndResponse)
+                this.setState({
+                    list: backEndResponse
+                })
+            })
+        }
+    
+    
+        removePlace = (placename) => {
+            //easy, just delete from DB!
+            console.log(this.props.login.email)
+            axios({
+                method: "POST",
+                url: `${window.apiHost}/culture/deleteFavePlace/${placename}`,
+                data: {
+                    email: this.props.login.email
+                }
+            }).then((backEndResponse) => {
+                // console.log(backEndResponse)
+                this.setState({
+                    list: backEndResponse
+                })
+            })
+        }
+       
+        render() {
+            // console.log(this.props)
+            if (this.state.list.data !== undefined) {
+
+                var favorites = this.state.list.data.map((culture, i) => {
+                    return (
+                        <div key={i} className="placeCard">
+                            <h4>{culture.placename}</h4>
+                            <div>
+                                <p>{culture.note}</p>
+                            </div>
+                            <div className="buttonContainer">
+                            <Button clicked={() => this.setState({ showAlert: true })} className="reviewButton">Review</Button>
+                            <Button clicked={() => this.editPlace(culture.placename)} className="editButton">Edit</Button>
+                            <Button clicked={() => this.removePlace(culture.placename)} className="deleteButton">Remove</Button>
+                            </div> 
+                        </div>
+                    )
+                })
+            }
+            
+    
+            return (
+                <div className="CultureFavorites">
+                    <h2>Favorites</h2>
+                    <AddForm
+                        addNewPlace={this.addNewPlace}
+                        placeholder="Add new..."
+                        textType="Add note..."
+                        defaultType="Restaurant"
+                        type2="Cafe"
+                        type3="Bar"
+                        type4="Diner"
+                    
+                    />
+                    <PlaceCards cards={favorites}/>
+
+                </div>
+            )
         }
     }
-    render() {
-        return (
-            <div className="CultureFavorites">
-                <div>
-                    <form onSubmit={this.addCultureFavorite} className="FavoriteForm">
-                        <input onChange={this.changeCultureFave} type="text" id="NewFave" placeholder="Add favorite..." value={this.state.favorite} />
-                        <input onChange={this.changeCultureNote} type="text" id="Note" placeholder="Remember for next time..." value={this.state.note} />
-                        <select className="Dropdown CultureType">
-                            <option default value="Museum/Gallery" onClick={}>Museum/Gallery</option>
-                            <option value="Performance" onClick={}>Performance</option>
-                            <option value="Movies/Music" onClick={}>Movies/Music</option>
-                            <option value="Literature/Learning" onClick={}>Literature/Learning</option>
-                        </select>
-                    </form>
-                </div>
-                <div>
-                    <table className="Table">
-                        <thead>
-                            <tr>
-                                <th>Place</th>
-                                <th>Type</th>
-                                <th>Note</th>
-                                <th>Remove</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* JSX for mapped array of activities! */}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        )
+    
+    function mapStateToProps(state) {
+        return {
+            login: state.login
+        }
     }
-}
-
-
-export default CultureFavorites;
+    
+    export default connect(mapStateToProps, null)(CultureFavorites);
