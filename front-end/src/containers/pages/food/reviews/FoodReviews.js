fe0a3bc6360f83124bee52bf2ab684446c14f893
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-07 21:24:01 -0500

    tried messing around with sweet alert and mopre css for food review

--- front-end/src/containers/pages/food/reviews/FoodReviews.js
+++ front-end/src/containers/pages/food/reviews/FoodReviews.js
@@ -106,8 +106,9 @@ class FoodReviews extends Component {
                 <h2>Reviews</h2>
                 <SweetAlert
                     show={this.state.showAlert}
-                    title="Added to Faves"
+                    title="Review Added"
                     text={this.state.msg}
+                    confirmBtnBsStyle="danger"
                     onConfirm={() => this.setState({ showAlert: false })}
                 />
                 <AddReviewForm
